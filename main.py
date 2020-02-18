ac5f2855ceec8127c24be4f9f6e8a0d3508fabb8
BlackTecno <blacktecnox@gmail.com>
2020-02-17 20:07:00 -0600

    still running into an error with attack speed vs attack speed offset, but now this runs in parallel, so that's cool

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json
 import http.client
 import requests
+import multiprocessing as par
 from RA import championList
 from RA import calc
 from RA import helpers
-
 
 key = 'RGAPI-3d17a56d-f9d4-471d-a14d-a5c26b64369f'
 
@@ -12,7 +12,10 @@ print('Starting Up')
 
 patchList = helpers.patchList()
 # helpers.grabHistory(patchList)
-calc.baseStats(patchList)
+
+if __name__ == '__main__':
+    p = par.Pool(8)
+    p.map(calc.avgStats, patchList)
 
 # list = championList.getList()
 # champInfo = championList.allChampInfo(list)
